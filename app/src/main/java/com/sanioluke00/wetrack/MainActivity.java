e6b850de68cf4c2d72b96978d5d08663cd7bfe7d
Sanio Luke <sanioluke00@gmail.com>
2022-09-26 18:40:52 +0530

    First Commit - WeTrack Project Creation

---
+++ app/src/main/java/com/sanioluke00/wetrack/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sanioluke00.wetrack;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
