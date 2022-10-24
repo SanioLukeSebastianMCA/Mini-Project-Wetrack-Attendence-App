19a5c14742c94da3e387ba4c57b05ed4e0804191
SanioLukeSebastianMCA <saniolukesebastian2023b@mca.ajce.in>
2022-10-25 00:15:58 +0530

    WeTrack App - MipMap Updations

--- app/src/main/java/com/sanioluke00/wetrack/Activities/AccountSelectionActivity.java
+++ app/src/main/java/com/sanioluke00/wetrack/Activities/AccountSelectionActivity.java
@@ -12,7 +12,5 @@ public class AccountSelectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_selection);
-
-
     }
 }
